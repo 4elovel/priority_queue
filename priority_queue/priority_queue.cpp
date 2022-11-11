7d7c182e963ec392a25c13339d0c77ae8c824a96
4elovel <marcynakvova007@gmail.com>
2022-11-12 01:00:19 +0300

    Добавьте файлы проекта.

---
+++ priority_queue/priority_queue.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void print(priority_queue < int> pq) {
+	for (size_t i = 0,j = pq.size(); i < j; i++)
+	{
+		cout << pq.top() << "\n";
+		pq.pop();
+	}
+	cout << endl;
+}
+
+int main()
+{
+	srand(time(NULL));
+    priority_queue < int> pq;
+	for (size_t i = 0; i < 25; i++)
+	{
+		pq.push(rand() % 1000);
+	}
+	print(pq);
+
+}
